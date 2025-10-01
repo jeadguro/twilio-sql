88c816e0ea2240f1dcb497d7b3e4d67c5662983c
jeadguro <jeadguro99@gmail.com>
2025-10-01 12:08:45 -0700

    Update database.py

--- taller/taller/database.py
+++ taller/taller/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:tu_password@localhost:5432/taller"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://usuario:password@localhost/taller"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
